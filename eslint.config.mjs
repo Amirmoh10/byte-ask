e1b5a998a93143ffcd54923ab3f1795bd67ac368
Amir Ghezala <mohamedghezala@gmail.com>
2025-10-25 21:48:04 +0200

    add eslint-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,30 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // External packages (node_modules) - React and other third-party libraries
+            ["^@?\\w"],
+            // Local imports (relative paths starting with . or ..)
+            ["^\\.", "^\\u0000"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
